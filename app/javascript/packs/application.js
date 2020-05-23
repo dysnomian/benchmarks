ae71d981d2f0af0eb42cd207027d518c70901e5e
Carl Jackson <carl.jackson@cloudcity.io>
2020-05-22 18:15:28 -0700

    wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,7 +32,7 @@ $(".benchmark-document .callout-with-icon svg.bar-chart path").attr(
   "scale(2)"
 )
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true)
+var componentRequireContext = require.context("src/components", true)
 var ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(componentRequireContext)
 ReactRailsUJS.mountComponents()
